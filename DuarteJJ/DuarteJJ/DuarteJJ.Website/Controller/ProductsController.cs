354f1462c2661c4764b12f0faa0ae1628622b2a8
JhazperDuarte <hiragalouise21@gmail.com>
2019-11-26 13:30:15 +0800

    initial Commit

---
+++ DuarteJJ/DuarteJJ/DuarteJJ.Website/Controller/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DuarteJJ.Website.Models;
+using DuarteJJ.Website.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DuarteJJ.Website.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService) 
+        {
+            this.ProductService = ProductService;
+        }
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+                return Ok();
+        }
+    }
+}
